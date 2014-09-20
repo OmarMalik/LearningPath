8f1177f8d19c9674a2c5eec0b5d7682d4464d078
OmarMalik <omaramalik94@gmail.com>
2014-09-20 10:06:16 -0400

    basic browse page that just shows all topics

--- lpath/views.py
+++ lpath/views.py
@@ -1,11 +1,13 @@
 from django.template import RequestContext
-from django.shortcuts import render_to_response
-
+from django.shortcuts import render_to_response, render
+from lpath.models import Topic
 
 def index(request):
     context = RequestContext(request)
     return render_to_response('lpath/index.html', context)
 
+
 def browse(request):
-    context = RequestContext(request)
-    return render_to_response('lpath/browse.html', context)
+    topics = Topic.objects.all().order_by('name')
+    context = {'topics': topics}
+    return render(request, 'lpath/browse.html', context)
